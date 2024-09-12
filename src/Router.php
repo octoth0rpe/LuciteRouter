c8fababd060424abf3f666741f2145b839957e6f
Mike Thorn <mthorn.work@gmail.com>
2024-09-12 15:11:43 -0400

    More fiddling with router api

--- src/Router.php
+++ src/Router.php
@@ -6,58 +6,57 @@ namespace Lucite\Router;
 
 class Router
 {
-    protected string $separator;
     protected $paths = [];
 
-    public function __construct(string $separator = ':')
+    protected function setupRoute(string $path)
     {
-        $this->separator = $separator;
+        if (isset($this->paths[$path]) === false) {
+            $this->paths[$path] = [];
+        }
     }
 
     public function get(string $path, string $handler): Router
     {
-        $this->paths[$path] = [['GET'], $handler];
+        $this->setupRoute($path);
+        $this->paths[$path]['GET'] = $handler;
         return $this;
     }
 
     public function post(string $path, string $handler): Router
     {
-        $this->paths[$path] = [['POST'], $handler];
+        $this->setupRoute($path);
+        $this->paths[$path]['POST'] = $handler;
         return $this;
     }
 
     public function patch(string $path, string $handler): Router
     {
-        $this->paths[$path] = [['PATCH'], $handler];
+        $this->setupRoute($path);
+        $this->paths[$path]['PATCH'] = $handler;
         return $this;
     }
 
     public function delete(string $path, string $handler): Router
     {
-        $this->paths[$path] = [['DELETE'], $handler];
+        $this->setupRoute($path);
+        $this->paths[$path]['DELETE'] = $handler;
         return $this;
     }
 
     public function map(string $path, array $methods, string $handler): Router
     {
-        $this->paths[$path] = [$methods, $handler];
+        $this->setupRoute($path);
+        foreach ($methods as $method) {
+            $this->paths[$path][$method] = $handler;
+        }
         return $this;
     }
 
     public function determineRoute(string $method, string $path): array
     {
-        if (isset($this->paths[$path])) {
-            $details = $this->paths[$path];
-            if (in_array($method, $details[0])) {
-                $parts = explode($this->separator, $details[1]);
-                if (count($parts) !== 2) {
-                    throw new InvalidRouteException($details[1], $this->separator);
-                }
-                return [
-                    'class' => $parts[0],
-                    'method' => $parts[1],
-                ];
-            }
+        $handler = ($this->paths[$path] ?? [])[$method] ?? null;
+        if ($handler !== null) {
+            return [$handler, strtolower($method)];
         }
         throw new UnknownRouteException($method, $path);
     }
--- tests/BasicRouteTest.php
+++ tests/BasicRouteTest.php
@@ -11,25 +11,30 @@ final class BasicRouteTest extends TestCase
     public function testCanMapGetRequest(): void
     {
         $router = new Router();
-        $router->get('/books', 'HandlerClass:handlerMethod');
+        $router->get('/books', 'HandlerClass');
         $handler = $router->determineRoute('GET', '/books');
-        $this->assertSame($handler['class'], 'HandlerClass');
-        $this->assertSame($handler['method'], 'handlerMethod');
+        $this->assertSame($handler[0], 'HandlerClass');
+        $this->assertSame($handler[1], 'get');
     }
 
     public function testThrowsExceptionForUnknownRoute(): void
     {
         $this->expectException(UnknownRouteException::class);
         $router = new Router();
-        $router->determineRoute('GET', '/books');
+        $router->determineRoute('POST', '/books');
     }
-
-    public function testAllowsOverridingSeparatorRoute(): void
+    public function testRegisterMultipleMethodsIndividually(): void
     {
-        $router = new Router('->');
-        $router->get('/books', 'HandlerClass->handlerMethod');
+        $router = new Router();
+        $router->get('/books', 'HandlerClass');
+        $router->post('/books', 'HandlerClass');
+
+        $handler = $router->determineRoute('POST', '/books');
+        $this->assertSame($handler[0], 'HandlerClass');
+        $this->assertSame($handler[1], 'post');
+
         $handler = $router->determineRoute('GET', '/books');
-        $this->assertSame($handler['class'], 'HandlerClass');
-        $this->assertSame($handler['method'], 'handlerMethod');
+        $this->assertSame($handler[0], 'HandlerClass');
+        $this->assertSame($handler[1], 'get');
     }
 }
